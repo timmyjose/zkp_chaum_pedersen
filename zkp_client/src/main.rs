2077c0099f665c8d80f6f948194ae53b14ae2881
Timmy Jose <zoltan.jose@gmail.com>
2023-04-27 17:57:41 +0530

    Added stubs for the ZKP client

--- zkp_client/src/main.rs
+++ zkp_client/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello from zkp_client");
+use zkp_auth::auth_client::AuthClient;
+use zkp_auth::{RegisterRequest, RegisterResponse};
+
+pub mod zkp_auth {
+    tonic::include_proto!("zkp_auth");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut auth_client = AuthClient::connect("http://127.0.0.1:9999").await?;
+
+    let request = tonic::Request::new(RegisterRequest {
+        user: "dummy user".into(),
+        y1: 54321,
+        y2: 54321,
+    });
+
+    let response = auth_client.register(request).await?;
+    println!("{response:?}");
+
+    Ok(())
 }
