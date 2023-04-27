fbc55a59e05efba91837c3120abe3f63532d86b1
Timmy Jose <zoltan.jose@gmail.com>
2023-04-27 17:37:27 +0530

    Added stubs for the ZKP server

--- zkp_server/src/lib.rs
+++ zkp_server/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use tonic::{transport::Server, Request, Response, Status};
+
+use crate::zkp_auth::{
+    auth_server::{Auth, AuthServer},
+    AuthenticationAnswerRequest, AuthenticationAnswerResponse, AuthenticationChallengeRequest,
+    AuthenticationChallengeResponse, RegisterRequest, RegisterResponse,
+};
+
+pub mod zkp_auth {
+    tonic::include_proto!("zkp_auth");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Default)]
+pub struct AuthImpl {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[tonic::async_trait]
+impl Auth for AuthImpl {
+    /// Register the user with the ZKP verifier
+    async fn register(
+        &self,
+        request: Request<RegisterRequest>,
+    ) -> Result<Response<RegisterResponse>, Status> {
+        println!("Got a register request: {request:?}");
+
+        let reply = zkp_auth::RegisterResponse {};
+
+        Ok(Response::new(reply))
+    }
+
+    /// Create an authentication challenge for the ZKP Prover
+    async fn create_authentication_challenge(
+        &self,
+        request: Request<AuthenticationChallengeRequest>,
+    ) -> Result<Response<AuthenticationChallengeResponse>, Status> {
+        println!("Got an authentication challenge request: {request:?}");
+
+        let reply = zkp_auth::AuthenticationChallengeResponse {
+            auth_id: "fake auth string".into(),
+            c: 12345,
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    /// Verify the authentication challenge received from the ZKP Prover
+    async fn verify_authentication(
+        &self,
+        request: Request<AuthenticationAnswerRequest>,
+    ) -> Result<Response<AuthenticationAnswerResponse>, Status> {
+        println!("Got an authentication answer request: {request:?}");
+
+        let reply = zkp_auth::AuthenticationAnswerResponse {
+            session_id: "fake session id".into(),
+        };
+
+        Ok(Response::new(reply))
     }
 }
--- zkp_server/src/main.rs
+++ zkp_server/src/main.rs
@@ -1,5 +1,23 @@
-fn main() {
-    loop {
-        println!("Hello from zkp_server");
-    }
+use tonic::{transport::Server, Request, Response, Status};
+
+use zkp_server::{
+    zkp_auth::{
+        auth_server::{Auth, AuthServer},
+        RegisterRequest, RegisterResponse,
+    },
+    AuthImpl,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let address = "127.0.0.1:9999".parse()?;
+
+    let auth_server = AuthImpl::default();
+
+    Server::builder()
+        .add_service(AuthServer::new(auth_server))
+        .serve(address)
+        .await?;
+
+    Ok(())
 }
